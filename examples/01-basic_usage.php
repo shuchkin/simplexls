ca66242fc983494f290dd8490af9d9353b713048
kijato <kijato@gmail.com>
2024-09-28 22:27:57 +0200

    Update 01-basic_usage.php

--- examples/01-basic_usage.php
+++ examples/01-basic_usage.php
@@ -5,7 +5,13 @@ use Shuchkin\SimpleXLS;
 
 echo '<h1>Parse books.xls</h1><pre>';
 if ($xls = SimpleXLS::parse('books.xls')) {
-    print_r($xls->rows());
+    #print_r($xls->rows());
+    for ($sheet = 0; $sheet < count($xls->sheetNames()); $sheet++) {
+		print "<h3>";
+		print_r($xls->boundsheets[$sheet]['name']);
+		print "</h3>";
+		print_r($xls->rows($sheet));
+	}
 } else {
     echo SimpleXLS::parseError();
 }
